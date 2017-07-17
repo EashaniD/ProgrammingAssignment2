8d4e26a276abd185718e4e777a6f8aed833cae36
Eashani <eashanimdeorukhkar@gmail.com>
2017-07-17 18:33:10 +0530

    Add existing file

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,22 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = numeric()) {
+			n<-sqrt(length(x))
+			x<-matrix(x,nrow=n,ncol=n,byrow=TRUE)
+                  matinv <- NULL
+                  setmat <- function(y){
+                                x <<- y
+					  n<-sqrt(length(x))
+					  x<-matrix(x,nrow=n,ncol=n,byrow=TRUE)
+
+                                matinv <<- NULL
+                  
+                    }
+                    getmat <- function() x
+                    setinv <- function(inv) matinv <<- inv
+                    getinv <- function() matinv
+                    list(setmat=setmat,getmat=getmat,setinv=setinv,getinv=getinv)
 
 }
 
@@ -12,4 +27,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+		    matinv <- x$getinv()
+                if(!is.null(matinv)){
+                  message("getting cached data")
+                  return(matinv)
+                }
+                  leng <- sqrt(length(x$getmat()))
+			data <- matrix(x$getmat(),nrow=leng,ncol=leng,byrow=TRUE)
+
+			matinv <- solve(data)
+			x$setinv(matinv)
+           		matinv
+                }
+
